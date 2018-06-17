ecba32a8a7cad13969d2437c88efd03e888c90b6
root <root@localhost.localdomain>
2018-06-17 01:54:12 -0400

    617727

--- app/views.py
+++ app/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django import views
 
-# Create your views here.
+def IndexView(request):
+    return HttpResponse("hello world")
--- mysite/settings.py
+++ mysite/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -76,8 +77,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME':'mysite',
+        'USER':'root',
+        'PASSWORD':'',
+        'HOST':'192.168.41.129',
+        'PORT':3306
     }
 }
 
