914f35ed28bd7a00b89e3224c2d20a3fb4b7f785
root <root@localhost.localdomain>
2018-06-20 11:29:47 -0400

    多对多正向查询和反向查询

--- app/models.py
+++ app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class AuthorInfo(models.Model):
+    name=models.CharField(max_length=100)
+    class Meta:
+        db_table='authorInfo'
+
+class BookInfo(models.Model):
+    title = models.CharField(max_length=100)
+    authors = models.ManyToManyField(AuthorInfo)
+    class Meta:
+        db_table="bookInfo"
--- app/views.py
+++ app/views.py
@@ -1,6 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time : 2018/6/18 下午10:48
+# @Author : bydong
+# @Site : 
+# @File : views.py
+# @Software: PyCharm
 from django.shortcuts import render
-from django.http import HttpResponse
-from django import views
+from models import *
+
 
 def IndexView(request):
-    return HttpResponse("hello world")
+
+    books=BookInfo.objects.all()
+    for book in books:
+        for b in book.authors.all():
+            print(b.name)
+
+    authors=AuthorInfo.objects.all()
+    for author in authors:
+        for book in author.bookinfo_set.all():
+            print(book.title)
+    context={"authors":authors,"books":books}
+    return render(request,'app/index.html',context)
